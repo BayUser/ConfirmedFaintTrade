9a2561796f34773a273ad58bb543d08d7f9fcbdc
BayUser <64269723+BayUser@users.noreply.github.com>
2022-05-07 18:12:44 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,165 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const jimp = require("jimp");
+const db = require("quick.db");
+const ayarlar = require("./ayarlar.json");
+const chalk = require("chalk");
+const fs = require("fs");
+const moment = require("moment");
+require("./util/eventLoader")(client);
+
+//-------------------- 7/24 Uptime --------------------//
+//-------------------- 7/24 Uptime --------------------//
+//-------------------- 7/24 Uptime --------------------//
+
+const express = require("express");
+const app = express();
+const http = require("http");
+app.get("/", (request, response) => {
+  console.log(`7/24 Hizmet Vermekteyim!`);
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
+//-------------------- 7/24 Uptime --------------------//
+//-------------------- 7/24 Uptime --------------------//
+//-------------------- 7/24 Uptime --------------------//
+
+client.on("ready", async () => {
+  client.appInfo = await client.fetchApplication();
+  setInterval(async () => {
+    client.appInfo = await client.fetchApplication();
+  }, 600);
+});
+
+
+const log = message => {
+  console.log(` ${message}`);
+};
+require("./util/eventLoader.js")(client);
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir("./komutlar/", (err, files) => {
+  if (err) console.error(err);
+  log(`${files.length} komut yüklenecek.`);
+  files.forEach(f => {
+    let props = require(`./komutlar/${f}`);
+    log(`Yüklenen komut: ${props.help.name}.`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+client.load = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+client.unload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+client.elevation = message => {
+  if (!message.guild) {
+    return;
+  }
+  let permlvl = 0;
+  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+  if (message.author.id === ayarlar.sahip) permlvl = 4;
+  return permlvl;
+};
+
+client.login(process.env.token);
+
+////////////////////////////////////////////Reklam Engel////////////////////////////////////////////
+client.on("message", msg => {
+ if(!db.has(`reklam_${msg.guild.id}`)) return;
+        const reklam = [".com", ".net", ".xyz", ".tk", ".pw", ".io", ".me", ".gg", "www.", "https", "http", ".gl", ".org", ".com.tr", ".biz", "net", ".rf.gd", ".az", ".party", "discord.gg",];
+        if (reklam.some(word => msg.content.includes(word))) {
+          try {
+            if (!msg.member.hasPermission("BAN_MEMBERS")) {
+                  msg.delete();
+                    return msg.reply('**🚫 Bu Sunucuda Reklam Yapmak Yasak.**').then(msg => msg.delete(3000));
+      
+ 
+            }              
+          } catch(err) {
+            console.log(err);
+          }
+        }
+    });
+//////////////////////Sa-as//////////////////////
+
+client.on('message', async (msg, member, guild) => {
+  let i = await  db.fetch(`saas_${msg.guild.id}`)
+      if(i === 'açık') {
+        if (msg.content.toLowerCase() === 'sa') {
+        msg.reply('**Aleyküm Selam Hoşgeldin.**');      
+      } 
+      }
+    });
+//////////////////////Sa-as//////////////////////
+
+
+client.on("message", msg => {
+ if(!db.has(`${msg.guild.id}.motion`)) return;
+        const kufur = ["salak","slk","mal","aptal","kahpe","orospu","sik kafalı","piç","Piç","aq","aw","Aq","Mal","ananınaı","yarrak","amk","sürtük","Salak","Mal","Sik","Amcık","amcık"];
+        if (kufur.some(word => msg.content.includes(word))) {
+          try {
+            if (!msg.member.hasPermission("BAN_MEMBERS")) {
+                  msg.delete();
+                    return msg.reply('**🚫 Bu Sunucuda Küfür Etmek Yasak.**').then(msg => msg.delete(3000));
+      
+ 
+            }              
+          } catch(err) {
+            console.log(err);
+          }
+        }
+    });
+
